ca17abfc6e14974afe7304a85bf0ea02f34b6d75
Brandon Adonis <ac104133@avcol.school.nz>
2021-09-03 10:08:07 +1200

    Add project files.

---
+++ FeedingTime/Models/Foods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+    namespace FeedingTime.Models
+    {
+        public class Food
+        {
+            public int FoodID { get; set; }
+
+            [Required]
+            [Display(Name = "Food Name")]
+            [StringLength(50)]
+            public string FoodName { get; set; }
+
+            [Required]
+            [Display(Name = "Food Type")]
+            [StringLength(50)]
+            public string FoodType { get; set; }
+
+            [Required]
+            [Display(Name = "Num Of Nutrients")]
+            [StringLength(50)]
+            public string Nutrients { get; set; }
+
+            [Required]
+            [Display(Name = "Allergies Ingredients")]
+            [StringLength(50)]
+            public string AllergiesIngredients { get; set; }
+
+
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            [Display(Name = "Expiry Date")]
+            public DateTime ExpiryDate { get; set; }
+
+
+            public Animals Animal { get; set; }
+
+
+
+        }
+    }
+}
---
+++ FeedingTime/Models/StaffMembers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedingTime.Models
+{
+    public class StaffMembers
+    {
+        public int StaffMembersID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Work Area")]
+        [StringLength(50)]
+        public string EnclosureWorkingAt { get; set; }
+
+        [Required]
+        [Display(Name = "Sex")]
+        public string Sex { get; set; }
+
+        [Required]
+        [Display(Name = "Roles")]
+        [StringLength(50)]
+        public string Roles { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Employed")]
+        public DateTime DateEmployed { get; set; }
+
+        public int PhoneNum { get; set; }
+
+
+        public Animals Animal { get; set; }
+
+    }
+}
+
+
+
+
+
